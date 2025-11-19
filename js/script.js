beea0e29168fd898b8a76b2d19706f2c3a717054
carminerizzi91-spec <carminerizzi91@gmail.com>
2025-11-19 12:28:27 +0100

    add: rotazione

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function loadPhoto() {
 function createCardHtml(photo) {
     return `
         <div class="col-12 col-md-4 col-lg-3 mb-5">
-            <div class="card-polaroid rotatable-card" data-rotation-state="normal">
+            <div class="card-polaroid rotatable-card">
                 <div class="pin"></div>
                 <div class="photo">
                     <img src="${photo.url}" alt="${photo.title}" class="img-fluid" style="border-radius: 4px; aspect-ratio: 1 / 1; object-fit: cover;">
@@ -46,5 +46,20 @@ function createCardHtml(photo) {
 }
 
 // Funzione per far ruotare la card
+
+function addRotationListeners() {
+    const cards = document.querySelectorAll('.rotatable-card');
+
+    cards.forEach(card => {
+        card.style.transition = 'transform 0.3s ease-out';
+
+        card.addEventListener('click', function () {
+            let rotationState = this.dataset.rotationState;
+
+            const randomAngle = (Math.random() * 20 - 10).toFixed(1);
+            this.style.transform = `rotate(${randomAngle}deg)`;
+        });
+    });
+}
 
 loadPhoto();
